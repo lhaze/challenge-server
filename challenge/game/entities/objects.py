0ad1b8d4ff15aa2a14ec6ccd3c9230f312ae32fe
Lukasz Haze <lhaze@lhaze.name>
2015-01-22 02:28:28 +0100

    Snake representations: HHOT & Tile; tests & pep8ify

--- challenge/game/entities/objects.py
+++ challenge/game/entities/objects.py
@@ -1,22 +1,22 @@
 from collections import deque, namedtuple, Sequence
-from enum import IntEnum, unique
-from itertools import izip, tee
+from enum import IntEnum
+import itertools
 
 from .orders import Turn
 
 
 class Direction(IntEnum):
     """
-    Enumeration type describing four directions of the world: north, east, south
-    and west. Representation is numerical for the sake of
+    Enumeration type describing four directions of the world: north, east,
+    south and west. Representation is numerical for the sake of
     """
     NORTH = 0
     EAST = 1
     SOUTH = 2
     WEST = 3
 
-    # Enum/IntEnum can have only __dunder__ attributes as the ones not processed
-    # by their Enum.__new__
+    # Enum/IntEnum can have only __dunder__ attributes as the ones not
+    # processed by their Enum.__new__
     __SYMBOL_2_VALUE_MAP__ = {
         'N': 0,
         'E': 1,
@@ -24,22 +24,37 @@ class Direction(IntEnum):
         'W': 3,
     }
 
+    __VALUE_2_SYMBOL_MAP__ = {
+        0: 'N',
+        1: 'E',
+        2: 'S',
+        3: 'W',
+    }
+
     # FIXME accessing Turn by their values seems a naive way, but it is a
     # workaround around other INSTANCE of Turn type here. Turn instances
     # can't be used as keys, else you're going to encounter a KeyError on
     # Direction.make_turn method. This seems to be a enum34 bug.
     __TURN_2_ADDEND_MAP__ = {
-        None: 0,  # No turn is interpreted like the Turn.FORWARD!
         'F': 0,
         'R': 1,
         'L': -1,
     }
 
+    # Keys are difference between 'other' (minuend; the 'next' direction) and
+    # 'me' (subtrahend; the previous direction).
+    __DIFFERENCE_2_TURN_MAP__ = {
+        -1: 'L',
+        0: 'F',
+        1: 'R',
+        3: 'L',  # -1 mod 4
+    }
 
     @classmethod
     def from_symbol(cls, symbol):
         """
-        Returns a Direction instance from its common symbol: one of [N, S, W, E]
+        Returns a Direction instance from its common symbol:
+        one of [N, S, W, E]
 
         It has to be a class factory method (not a __new__), because it is
         the way that Enum works in enum34 (no custom __new__ after Enum
@@ -49,9 +64,15 @@ class Direction(IntEnum):
             value = cls.__SYMBOL_2_VALUE_MAP__[symbol]
         except KeyError:
             raise ValueError(
-                "Invalid Direction symbol '{}'. Accepted values are: {}.". \
+                "Invalid Direction symbol '{}'. Accepted values are: {}.".
                 format(symbol, ", ".join(cls.__SYMBOL_2_VALUE_MAP__.keys())))
         return cls(value)
+
+    def to_symbol(self):
+        """
+        Returns a common symbol of the direction: one of [N, S, W, E].
+        """
+        return self.__VALUE_2_SYMBOL_MAP__[self.value]
 
     def make_turn(self, turn):
         """
@@ -71,6 +92,22 @@ class Direction(IntEnum):
                 "Invalid turn choice '{}'. Accepted values are Turn instances "
                 "or None.").format(turn))
         return Direction((self + addend) % 4)
+
+    def get_turn_to(self, direction):
+        """
+        Returns:
+            Turn instance representing a turn which you have to take to make
+            'self' change into 'direction'.
+        Raises:
+            ValueError iff it can't be done (ie. turning N into S can't be done
+            in a single turn).
+        """
+        symbol = self.__DIFFERENCE_2_TURN_MAP__.get((direction - self) % 4)
+        if symbol is None:
+            raise ValueError((
+                "Turning from {} to {} can't be done in a single "
+                "turn.").format(self.value, direction.value))
+        return Turn(symbol)
 
     @property
     def opposite(self):
@@ -99,6 +136,7 @@ class Tile(namedtuple('Tile', ('x', 'y'))):
     }
 
     def __new__(cls, x, y):
+        """ Casts arguments to int """
         return super(Tile, cls).__new__(cls, int(x), int(y))
 
     def __add__(self, vector):
@@ -120,8 +158,8 @@ class Tile(namedtuple('Tile', ('x', 'y'))):
             other_y = int(vector[1])
         except TypeError:
             raise ValueError(
-                "Value '{}' seems to be invalid. Doesn't cast to int.".format(
-                vector))
+                "Value '{}' seems to be invalid. Can't be cast to int.".
+                format(vector))
         x = self[0] + other_x
         y = self[1] + other_y
         return Tile(x, y)
@@ -129,12 +167,13 @@ class Tile(namedtuple('Tile', ('x', 'y'))):
     def __sub__(self, other):
         """
         Returns vector (2-tuple of positional addition) between this tile and
-        the other one. The self is interpreted as the *end* of the vector, while
-        the 'other' is its beginning.
+        the other one. The self is interpreted as the *end* of the vector,
+        while the 'other' is its beginning.
         """
         if not isinstance(other, Tile):
             raise ValueError(
-                "Tile supports only positional subtraction of an Tile instance")
+                "Tile supports only positional subtraction of an Tile "
+                "instance")
         return (self.x - other.x, self.y - other.y)
 
     def get_adjacent(self, direction):
@@ -171,8 +210,9 @@ class Tile(namedtuple('Tile', ('x', 'y'))):
         return self.VECTOR_2_DIRECTION_MAP.get(vector)
 
     def is_adjacent(self, other):
+        """ Checks iff this tile is adjacent to the other """
         vector = other - self
-        return (vector in self.VECTOR_2_DIRECTION_MAP.keys())
+        return vector in self.VECTOR_2_DIRECTION_MAP.keys()
 
     def to_short_repr(self):
         """
@@ -180,6 +220,13 @@ class Tile(namedtuple('Tile', ('x', 'y'))):
             <int>,<int>
         """
         return ",".join((str(self[0]), str(self[1])))
+
+
+def _iterate_pairwise(sequence):
+    """ Returns iterable which iterates by pairs """
+    a, b = itertools.tee(sequence)
+    next(b, None)
+    return itertools.izip(a, b)
 
 
 class Snake(deque):
@@ -194,12 +241,37 @@ class Snake(deque):
     """
 
     def __init__(self, tiles):
-        super(Snake, self).__init__(tiles)
-        if len(tiles) < 3:
+        if len(tiles) < 2:
             raise ValueError((
-            "Snake should have at least 3 tiles. Input sequence {} has only {}"
-            " tiles").format(tiles, len(tiles)))
+                "Snake should have at least 2 tiles. Input sequence {} has "
+                "only {} tiles").format(tiles, len(tiles)))
+        super(Snake, self).__init__(tiles)
         self.heading = tiles[1].get_direction_to(tiles[0])
+
+    def __str__(self):
+        return self.to_tile_form()
+
+    @classmethod
+    def from_tile_form(cls, snake_repr):
+        """
+        Creates instance from stringified sequence of turns.
+
+        BNF of the tiles form:
+        <tile> ::= <number> "," <number>
+        <tile_sequence> ::= <tile_sequence> ":" <tile> | <tile> ":" <tile>
+        <snake_repr> ::= <tile_sequence>
+        """
+        tiles_repr = snake_repr.split(':')
+        tiles = [Tile(*tile_repr.split(',')) for tile_repr in tiles_repr]
+        return cls(tiles)
+
+    def to_tile_form(self):
+        """
+        Returns a stringified Snake in the form of sequence of tiles separated
+        with colons. Definition of the form is in the 'form_tile_form' method
+        docstring.
+        """
+        return ":".join(["%s,%s" % (tile.x, tile.y) for tile in self])
 
     @classmethod
     def from_hhot_form(cls, snake_repr):
@@ -210,11 +282,11 @@ class Snake(deque):
         of commands.
 
         NOTE 1: Reversed sequence of opposited turns is just a history of snake
-        turns (hence the 'opposite' concept in the name).
+            turns (hence the 'opposite' concept in the name).
 
         NOTE 2: Definition states there is at least one turn, so the snake
-        should have at least 3 tiles (head, heading-determined and
-        turn-determined).
+            should have at least 3 tiles (head, heading-determined and
+            turn-determined).
 
         BNF of the HHOT form:
         <tile> ::= <number> "," <number>
@@ -222,18 +294,25 @@ class Snake(deque):
         <heading> ::= "N" | "E" | "S" | "W"  # Direction.NORTH/EAST/SOUTH/WEST
         <turn> ::= "F" | "L" | "R"  # Turn.FORWARD/LEFT/RIGHT
         <turn_sequence> ::= <turn> <turn_sequence> | <turn>
-        <snake_repr> ::= <head> ":" <heading> ":" <turn_sequence>
+        <snake_repr> ::= <head> ":" <heading> <turn_sequence>
+
+        For example:
+            1,1:ERLFF
         """
-        head, heading_symbol, turn_sequence = snake_repr.split(':')
-        tile = Tile(*head.split(','))
-        direction = Direction.from_symbol(heading_symbol).opposite
-        sequence = deque((tile,))
-        for turn_symbol in turn_sequence:
+        head_repr, body_repr = snake_repr.split(':')
+        # assuming at least one symbol present, which will be the heading
+        # while the rest of the body will be interpreted as turns
+        assert len(body_repr)
+        direction = Direction.from_symbol(body_repr[0]).opposite
+        head = Tile(*head_repr.split(','))
+        tile = head.get_adjacent(direction)
+        snake = deque((head, tile))
+        for turn_symbol in body_repr[1:]:
             turn = Turn(turn_symbol)
             direction = direction.make_turn(turn)
             tile = tile.get_adjacent(direction)
-            sequence.append(tile)
-        return cls(sequence)
+            snake.append(tile)
+        return cls(snake)
 
     def to_hhot_form(self):
         """
@@ -242,12 +321,25 @@ class Snake(deque):
         You can find a definition of it in docstring of from_hhot_form factory
         method.
         """
-
-        sequence = []
-        # XXX turn self into sequence of values of turns
-        return "{head}:{heading}:{turn_sequence}".format(
-            head=self.head.to_short_repr(), heading=self.heading,
-            turn_sequence=sequence)
+        # iterate over tiles, evaluate Directions (using pair of adjacent
+        # tiles) and turn them to Turns (using pairs of sequential Directions)
+        tiles_iterator = iter(self)
+        next(tiles_iterator)
+        old_tile = next(tiles_iterator)
+        old_direction = self.heading.opposite
+        turns = []
+        for tile in tiles_iterator:
+            direction = old_tile.get_direction_to(tile)
+            if direction is None:
+                raise ValueError((
+                    "The snake seems to be in an invalid state. Tile {} is "
+                    "not adjacent to the {} tile.").format(old_tile, tile))
+            turns.append(old_direction.get_turn_to(direction))
+            old_tile, old_direction = tile, direction
+        turns_repr = "".join([turn.value for turn in turns])
+        return "{head}:{heading}{turns_repr}".format(
+            head=self.head.to_short_repr(), heading=self.heading.to_symbol(),
+            turns_repr=turns_repr)
 
     @property
     def head(self):
@@ -262,7 +354,7 @@ class Snake(deque):
         """ The tail of the snake (its last tile), rightmost."""
         return self[-1]
 
-    def move(self, order, food=None):
+    def move(self, order, foods=None):
         self.heading = self.heading.turn(order)  # turn your head
         new_head = self[0].get_adjacent(self.heading)
         self.appendleft(new_head)  # move your head forward
@@ -277,33 +369,22 @@ class Snake(deque):
             * heading is consistent with the first two tiles.
         """
         return all((
-            self.is_non_intersecting,
-            self.is_consistent,
-            self.has_valid_heading))
+            self.is_non_intersecting(),
+            self.is_consistent(),
+            self.has_valid_heading()))
 
-    def get_tiles_pairwise(self):
-        """
-        Returns iterable which iterates by pairs of Snake's tiles.
-        """
-        a, b = itertools.tee(iterable)
-        next(b, None)
-        return itertools.izip(a, b)
-
-    @property
     def is_non_intersecting(self):
         """ True iff none of my tiles overlap """
         return len(self) == len(set(self))
 
-    @property
     def is_consistent(self):
         """ True iff all Snake's tiles are adjacent to their neighbours """
-        return all(x.is_adjacent(y) for x, y in self.get_tiles_pairwise())
+        return all(x.is_adjacent(y) for x, y in _iterate_pairwise(self))
 
-    @property
     def has_valid_heading(self):
         """
         True iff Snake is heading in the direction that my first two tiles
         define.
         """
         direction = self[1].get_direction_to(self[0])
-        return direction == heading
+        return direction == self.heading
--- challenge/game/test_game.py
+++ challenge/game/test_game.py
@@ -3,10 +3,8 @@ import pytest
 from .entities import Direction, Snake, Tile, Turn
 
 
-class TestTileDirection(object):
-    """
-    Tests of features with Tile-Direction interaction.
-    """
+class TestDirection(object):
+    """ Tests of Direction class """
 
     @pytest.mark.parametrize("direction, turn, result", [
         (Direction.NORTH, Turn.FORWARD, Direction.NORTH),
@@ -23,11 +21,41 @@ class TestTileDirection(object):
         (Direction.NORTH, Direction.SOUTH),
         (Direction.SOUTH, Direction.NORTH),
         (Direction.EAST, Direction.WEST),
-        (Direction.WEST, Direction.EAST)
+        (Direction.WEST, Direction.EAST),
     ])
     def test_opposite_direction(self, direction, result):
         " Test of opposite direction "
-        assert direction.opposite() == result
+        assert direction.opposite == result
+
+    def test_direction2symbol_bijection(self):
+        " Test of __VALUE_2_SYMBOL_MAP__/__SYMBOL_2_VALUE_MAP__ bijection "
+        V2S = Direction.__VALUE_2_SYMBOL_MAP__
+        S2V = Direction.__SYMBOL_2_VALUE_MAP__
+        for d in Direction:
+            assert d == S2V[V2S[d.value]]
+
+    @pytest.mark.parametrize("this_turn, other_direction, result", [
+        (Direction.NORTH, Direction.WEST, Turn.LEFT),
+        (Direction.NORTH, Direction.EAST, Turn.RIGHT),
+        (Direction.NORTH, Direction.NORTH, Turn.FORWARD),
+        (Direction.WEST, Direction.NORTH, Turn.RIGHT),
+        (Direction.WEST, Direction.SOUTH, Turn.LEFT),
+    ])
+    def test_get_turn_to(self, this_turn, other_direction, result):
+        " Test of Direction.get_turn_to "
+        assert this_turn.get_turn_to(other_direction) == result
+
+    def test_turn2addend_bijection(self):
+        " Test of __TURN_2_ADDEND_MAP__/__DIFFERENCE_2_TURN_MAP__ bijection "
+        T2A = Direction.__TURN_2_ADDEND_MAP__
+        D2T = Direction.__DIFFERENCE_2_TURN_MAP__
+        for t in Turn:
+            value = t.value
+            assert value == D2T[T2A[value]]
+
+
+class TestTile(object):
+    """ Tests of Tile class and Tile-Direction interations """
 
     @pytest.mark.parametrize("tile, direction, result", [
         (Tile(1, 1), Direction.NORTH, Tile(1, 2)),
@@ -46,31 +74,43 @@ class TestTileDirection(object):
         for vector in V2D.keys():
             assert vector == D2V[V2D[vector]]
 
+    @pytest.mark.parametrize("tile, other_tile", [
+        (Tile(1, 1), Tile(1, 2)),
+        (Tile(1, 1), Tile(2, 1)),
+        (Tile(1, 1), Tile(1, 0)),
+        (Tile(1, 1), Tile(0, 1)),
+    ])
+    def test_get_direction_to_bijection(self, tile, other_tile):
+        " Test of Tile.get_direction_to "
+        assert tile.get_adjacent(tile.get_direction_to(other_tile)) ==  \
+            other_tile
+
 
 class TestSnake(object):
     """
-    Tests assumptions about Snake objects.
+    Tests of Snake class and Snake-Order interactions.
     """
-
-    @pytest.mark.xfail
     @pytest.mark.parametrize("snake_repr", [
-        "5,5:N:FRLF",
-        "-1,-1:E:L",
-        "0,0:S:RR"
+        "5,5:NRLF",
+        "-1,-1:EL",
+        "0,0:SRR",
     ])
     def test_hhot2snake(self, snake_repr):
         " Test of transforming HHOT form to Snake instance and back "
         assert snake_repr == Snake.from_hhot_form(snake_repr).to_hhot_form()
 
-    @pytest.mark.parametrize("snake", [])
-    def test_snake2hhot(self, snake):
+    @pytest.mark.parametrize("snake_tile_repr", [
+        "5,5:5,6:6,6:7,6",
+        "1,1:2,1:2,2:1,2:1,1"
+    ])
+    def test_snake2hhot(self, snake_tile_repr):
         " Test of transforming Snake instance to HHOT and back "
+        snake = Snake.from_tile_form(snake_tile_repr)
         assert snake == Snake.from_hhot_form(snake.to_hhot_form())
 
     @pytest.mark.parametrize("snake, result", [
-        (Snake.from_hhot_form("1,1:S:RRR"), True)
+        (Snake.from_hhot_form("1,1:SRRR"), False)
     ])
-    def test_intersecting_snake(self, snake, result):
+    def test_non_intersecting_snake(self, snake, result):
         " Test of intersecting snake validation "
-        assert snake.is_non_intersecting == result
-
+        assert snake.is_non_intersecting() == result
